d823424b3d7a9f2bbce859d8ddb937e551aba9ef
Tanvir225 <tanvirrehman200@gmail.com>
2026-01-15 01:33:46 +0600

    cors issue soleve added frontend url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors(
     {
         origin: [
             "http://localhost:5173",
-            "tremendous-bike.surge.sh"
+            "https://tremendous-bike.surge.sh/"
         ],
         credentials: true,
     }
